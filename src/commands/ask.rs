bbe62011a9959e0146d901a15b0c26e85fd23aa5
Ethan Howard <ethanh20009@outlook.com>
2024-06-30 16:59:58 +0100

    answer question

--- src/commands/ask.rs
+++ src/commands/ask.rs
@@ -1,4 +1,6 @@
-use serenity::all::{CommandOptionType, CreateCommand, CreateCommandOption, ResolvedOption};
+use serenity::all::{
+    CommandOptionType, CreateCommand, CreateCommandOption, ResolvedOption, ResolvedValue,
+};
 
 use crate::environment::Environment;
 
@@ -20,12 +22,21 @@ pub fn register_ask(environment: &Environment) -> CreateCommand {
             .required(true),
         )
 }
-pub fn run_ask(_options: &[ResolvedOption]) -> Result<String> {
-    Ok("Hello".to_string())
+pub fn run_ask(options: &[ResolvedOption]) -> Result<String> {
+    let question_response = &options.first().ok_or(Error::MissingQuestion)?.value;
+    match question_response {
+        ResolvedValue::String(question) => Ok(format!(
+            "Question: {}\n\n{}",
+            question, "I don't think anything yet"
+        )),
+        _ => Err(Error::MissingQuestion.into()),
+    }
 }
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("Question unanswerable")]
     Unanswerable,
+    #[error("Missing question")]
+    MissingQuestion,
 }
